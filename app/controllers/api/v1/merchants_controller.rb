213b59df6e44d2b18c513da4960ab959c5cbcea8
austinandrade <austinmandrade@gmail.com>
2021-05-24 16:38:52 -0400

    Complete pagination and all 7 passing postman test cases

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,5 +1,23 @@
 class Api::V1::MerchantsController < ApplicationController
+  MAX_PAGINATION_LIMIT = 20
+
   def index
-    render json: Merchant.all
+    merchant = Merchant.offset(page_number).limit(per_page)
+    render json: MerchantSerializer.new(merchant)
+  end
+
+  def show
+    render json: MerchantSerializer.new(Merchant.find(params[:id]))
+  end
+
+  private
+
+  def per_page
+    params.fetch(:per_page, MAX_PAGINATION_LIMIT).to_i
+  end
+
+  def page_number
+    page = [params.fetch(:page, 1).to_i, 1].max
+    (page - 1) * per_page
   end
 end
--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -1,23 +1,81 @@
 require 'rails_helper'
 
 describe "Merchants API" do
-  it "sends a list of merchants" do
+  it "returns 3 merchant json objects" do
     create_list(:merchant, 3)
 
     get '/api/v1/merchants'
 
     expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.error?).to eq(false)
 
     merchants = JSON.parse(response.body, symbolize_names: true)
 
-    expect(merchants.count).to eq(3)
+    expect(merchants[:data].count).to eq(3)
+    expect(merchants).to have_key(:data)
+    expect(merchants[:data]).to be_an(Array)
 
-    merchants.each do |merchant|
+    merchants[:data].each do |merchant|
+      expect(merchant[:attributes]).to be_a(Hash)
+
       expect(merchant).to have_key(:id)
-      expect(merchant[:id]).to be_an(Integer)
+      expect(merchant[:id]).to be_an(String)
 
-      expect(merchant).to have_key(:name)
-      expect(merchant[:name]).to be_a(String)
+      expect(merchant[:attributes]).to have_key(:name)
+      expect(merchant[:attributes][:name]).to be_a(String)
     end
+  end
+
+  it "returns one merchant by its id" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.error?).to eq(false)
+
+    merchant = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchant).to have_key(:data)
+    expect(merchant[:data]).to be_a(Hash)
+    expect(merchant[:data][:attributes]).to be_a(Hash)
+
+    expect(merchant[:data]).to have_key(:id)
+    expect(merchant[:data][:id]).to be_a(String)
+    expect(merchant[:data][:id].to_i).to eq(id)
+
+    expect(merchant[:data][:attributes]).to have_key(:name)
+    expect(merchant[:data][:attributes][:name]).to be_a(String)
+  end
+
+  it "returns a subset of merchants based on per_page limitation" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants', params: {per_page: 1}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.error?).to eq(false)
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data].count).to eq(1)
+  end
+
+  it "returns a subset of merchants based on desired page limitation" do
+    merchant_collection = create_list(:merchant, 25)
+    twenty_first_merchant_id = merchant_collection[20].id
+
+    get '/api/v1/merchants', params: {page: 2}
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+    expect(response.error?).to eq(false)
+
+    merchants = JSON.parse(response.body, symbolize_names: true)
+
+    expect(merchants[:data][0][:id].to_i).to eq(twenty_first_merchant_id)
   end
 end
