a5e07b3bfd32c5ec2301e0c51623d4391717cb14
austinandrade <austinmandrade@gmail.com>
2021-05-25 23:31:15 -0400

    Add item_merchant route to retrieve an item's merchant

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: [:index, :show]
-      get "merchants/:id/items", to: 'merchants_items#index'
+      get "merchants/:id/items", to: 'merchant_items#index'
       resources :items
+      get "items/:id/merchant", to: 'item_merchant#show'
     end
   end
 end
