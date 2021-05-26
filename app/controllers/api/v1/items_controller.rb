1fd739fab7d4a79ad3887509d7e18d1646f0c8a6
austinandrade <austinmandrade@gmail.com>
2021-05-25 23:02:14 -0400

    Add all items crud actions

---
+++ app/controllers/api/v1/items_controller.rb
@@ -0,0 +1,37 @@
+class Api::V1::ItemsController < ApplicationController
+  include Pagination
+
+  def index
+    items = Item.offset(page_number).limit(per_page)
+    render json: ItemSerializer.new(items)
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: ItemSerializer.new(item)
+  end
+
+  def create
+    created_item = Item.create!(item_params)
+    render json: ItemSerializer.new(created_item), :status => 201
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      render json: ItemSerializer.new(item)
+    else
+      render :json => {:error =>  'Not Found'}.to_json, :status => 404
+    end
+  end
+
+  def destroy
+    Item.delete(params[:id])
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :unit_price, :merchant_id)
+  end
+end
