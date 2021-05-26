20d88f641e6386a14ae92a76acf0fbd0bb26781d
austinandrade <austinmandrade@gmail.com>
2021-05-26 11:33:08 -0400

    Add items search by partial name fragment route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get "merchants/find", to: 'search#find_merchant'
+      get "items/find_all", to: 'search#find_items'
       resources :merchants, only: [:index, :show]
       get "merchants/:id/items", to: 'merchant_items#index'
       resources :items
