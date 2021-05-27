c7b7d8a1988960707dc120f3e02fc533448b2192
austinandrade <austinmandrade@gmail.com>
2021-05-26 23:01:18 -0400

    Add top item selling merchants route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       get "items/find_all", to: 'search#find_items'
       get "revenue/merchants", to: 'calculations#top_merchants'
       get "revenue/merchants/:id", to: 'calculations#merchant_total_revenue'
+      get "merchants/most_items", to: 'calculations#top_item_selling_merchants'
       resources :merchants, only: [:index, :show]
       get "merchants/:id/items", to: 'merchant_items#index'
       resources :items
